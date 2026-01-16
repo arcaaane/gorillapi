acdc7d86a91572dc7ab4dc41a81af9d5fa2b7b9f
sudzy <tuckeralbert.le.e@gmail.com>
2026-01-15 18:40:04 -0600

    Create api.py

---
+++ api.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({"status": "A content packed API for all things Gorilla Tag!"})
+
+@app.route("/hello")
+def main():
+    return jsonify({"message": f"placeholder lol"})
+
+def handler(environ, start_response):
+    return app(environ, start_response)
